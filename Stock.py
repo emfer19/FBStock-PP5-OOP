aca1ed5d1ab54bd3a10bd6516d1eb08a0bd2d5b0
Emily Ferretti <ferrettiek@slu.edu>
2016-03-22 10:50:34 -0500

    class building

--- Stock.py
+++ Stock.py
@@ -18,6 +18,64 @@ class Stock:
       - Volume"""
 
   def __init__(self, date, openingV, highV, lowV, closingV, volume):
+    """create stock with given date, opening value, high value,
+       low value, closing value, and volume."""
+
     self._date=date
     self._openingV=openingV
-    self._highV
+    self._highV=highV
+    self._lowV=lowV
+    self._closingV=closingV
+    self._volume=volume
+
+  def setDate(self, date):
+    """Assign the received date of the Stock"""
+    self._date=date
+
+  def getDate(self):
+    """Return the date of the Stock"""
+    return self._date
+
+  def setOpeningVal(self, openingV):
+    """Assign the received opening value of the Stock"""
+    self._openingV=float(openingV)
+
+  def getOpeningVal(self):
+    """Return the opening value of the Stock"""
+    return self._openingV
+
+  def setHighVal(self, highV):
+    """Assign the received high value of the Stock"""
+    self._highV=float(highV)
+
+  def getHighVal(self):
+    """Return the high value of the Stock"""
+    return self._highV
+
+  def setLowVal(self, lowV):
+    """Assign the low value of the Stock"""
+    self._lowV=lowV
+
+  def getLowVal(self):
+    """Return the low value of the Stock"""
+    return self._lowV
+
+  def setClosingVal(self, closingV):
+    """Assign the closing value of the Stock"""
+    self._closingV=closingV
+
+  def getClosingVal(self):
+    """Return the closing value of the Stock"""
+    return self._closingV
+
+  def setVolume(self, volume):
+    """Assign the volume of the Stock"""
+    self._volume=volume
+
+  def getVolume(self):
+    """Return the volume of the Stock"""
+    return self._volume
+
+  def __str__(self):
+    """Return a string containing all the Stock data"""
+    display='Date: 
