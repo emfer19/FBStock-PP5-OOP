955c04f6200f462da44930ef057664c7be196f78
Emily Ferretti <ferrettiek@slu.edu>
2016-03-23 10:45:41 -0500

    start class build

--- FBStock.py
+++ FBStock.py
@@ -8,3 +8,21 @@
 
 '''Honor Code Statement: In keeping with the honor code policies of St.Louis University, the Department of Mathematics and Computer Science, I affirm that I have neither given nor received assistance on this programming assignment. This assignment represents my individual, original effort.'''
 
+from Stock import Stock
+
+class FBStock:
+  def __init__(self):
+    """receives nothing"""
+
+    self._fileobj=None
+    self._stocks=[]
+    
+  def openFile(self):
+    """Continuously ask for a name of the file containing the FB stock
+    information until it can be opened for reading """
+    while not self._fileObj:
+      filename=raw_input('Enter filename with Stock data: ')
+      try:
+	self._fileObj=file(filename)
+      except IOError:
+	print 'unable to open file',filename
