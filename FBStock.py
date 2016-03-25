771d31f6aae74a4da362dab9ffa935c6f86bb831
Emily Ferretti <emfer19@users.noreply.github.com>
2016-03-24 21:46:40 -0500

    class build

--- FBStock.py
+++ FBStock.py
@@ -26,3 +26,39 @@ class FBStock:
 	self._fileObj=file(filename)
       except IOError:
 	print 'unable to open file',filename
+
+  def get_data_list(self):
+    """Read file of stock data, create list of Stock objects"""
+    line=self._fileObj.readline() #read first line, trash because headers
+    line=self._fileObj.readline()
+    while line:
+      stockObj=Stock() #create Stock object
+      data=line.split() #split line read in
+
+      #assign parts to object
+      stockObj.setDate(data[0])
+      stockObj.setOpeningVal(float(data[1]))
+      stockObj.setHighVal(float(data[2]))
+      stockObj.setLowVal(float(data[3]))
+      stockObj.setClosingVal(float(data[4]))
+      stockObj.setVolume(int(data[5]))
+      self._stocks.append(stockObj)
+
+      line=self._fileObj.readline()
+
+  def average_data(self,year):
+    """Receive a year and find average of data for each column for year.
+
+    Will also calculate and display month within received year with 
+    the highest closing value, and month with the lowest closing value.
+      **In the case of a "tie", earliest month of that year will be shown."""
+    objects=[]
+    for i in len(self._stocks):
+      if str(year) in self._stocks[i].getDate():
+        objects.append()
+    print objects
+
+
+#unit testing
+if __name__=='__main__':
+  
