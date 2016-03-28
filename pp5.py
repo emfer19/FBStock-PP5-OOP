db136f5ac5aaf38cef7f1cba509538ec1cc0386a
Emily Ferretti <emfer19@users.noreply.github.com>
2016-03-28 18:51:32 -0500

    program build edit

--- pp5.py
+++ pp5.py
@@ -8,4 +8,27 @@
 
 '''Honor Code Statement: In keeping with the honor code policies of St.Louis University, the Department of Mathematics and Computer Science, I affirm that I have neither given nor received assistance on this programming assignment. This assignment represents my individual, original effort.'''
 
+#main program file
 
+from FBStock import *
+
+stocks=FBStock() #create FBStock instance
+stocks.openFile()
+stocks.get_data_list() #create stock data list
+
+
+#ask what year they want to find the average of
+year=0
+while not 2012<=year<=2016:
+  try:
+    year=int(raw_input('What year do you want averaged? '))
+    if not 2012<=year<=2016:
+      print 'Year must be between 2012 and 2016\n'
+  except ValueError:
+    print 'Please enter a valid year between 2012 and 2016\n'
+  except TypeError:
+    print 'Please enter a valid year\n'
+
+print
+stocks.average_data(year)
+raw_input('Press ENTER to end')
